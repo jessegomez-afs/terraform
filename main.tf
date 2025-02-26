20453d1383664d2b93fca3117b72ac3438ec85e5
jessegomez-afs <114248698+jessegomez-afs@users.noreply.github.com>
2025-02-26 11:08:32 -0600

    just create a vpc

---
+++ main.tf
@@ -0,0 +1,27 @@
+# built from:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+# and
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "jg-vpc"
+  }
+}
+
